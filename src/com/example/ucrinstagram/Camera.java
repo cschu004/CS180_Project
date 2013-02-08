d759c0463d9d0ae8fa5b8bbe1d26b0e7811af86c
Jacky Huang <Jacky@wireless-student-wpa-138-23-18-166.bulk.ucr.edu>
2013-02-08 09:55:15 -0800

    Updated

--- src/com/example/ucrinstagram/Camera.java
+++ src/com/example/ucrinstagram/Camera.java
@@ -1,22 +1,89 @@
 package com.example.ucrinstagram;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Camera extends Activity {
+    final int TAKE_PICTURE = 1;
+    final int ACTIVITY_SELECT_IMAGE = 1234;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_camera);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_camera, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_camera, menu);
+        return true;
+    }
+    
+    public void startCamera(View view){
+    	Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        //intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+    	startActivityForResult(intent, TAKE_PICTURE);   
+    }
 
+    public void startGallery(View view){
+	    Intent intent = new Intent();
+	    intent.setType("image/*");
+	    intent.setAction(Intent.ACTION_GET_CONTENT);//
+	    startActivityForResult(Intent.createChooser(intent, "Select Picture for UCRinstagram"),ACTIVITY_SELECT_IMAGE);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+    	Bitmap bmp = null;
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == TAKE_PICTURE) {
+	            Uri selectedImage = data.getData();
+	            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+	
+	            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+	            cursor.moveToFirst();
+	
+	            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+	            String filePath = cursor.getString(columnIndex);
+	            cursor.close();
+	        	 bmp = BitmapFactory.decodeFile(filePath);
+
+		        ImageView myImage2 = (ImageView) findViewById(R.id.imageView);
+		        myImage2.setImageBitmap(bmp);
+       }
+        else {
+                Toast.makeText(getBaseContext(), "Please capture again", Toast.LENGTH_LONG).show();
+        }
+
+
+        if(resultCode == RESULT_OK && requestCode == ACTIVITY_SELECT_IMAGE){  
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String filePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            Bitmap yourSelectedImage = BitmapFactory.decodeFile(filePath);
+	        ImageView myImage2 = (ImageView) findViewById(R.id.imageView);
+	        myImage2.setImageBitmap(yourSelectedImage);
+        }
+
+
+   }
 }
