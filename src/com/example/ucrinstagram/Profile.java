d8a4095d963ac833311c9d1d7d07bf2ffbdfe496
kevin <kgouw001@ucr.edu>
2013-02-08 16:45:22 -0800

    Profiles & Settings UI Almost Complete

--- src/com/example/ucrinstagram/Profile.java
+++ src/com/example/ucrinstagram/Profile.java
@@ -1,8 +1,11 @@
 package com.example.ucrinstagram;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Profile extends Activity {
 
@@ -10,6 +13,30 @@ public class Profile extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_profile);
+		
+        // Loader image - will be shown before loading image
+        int loader = R.drawable.loader;
+ 
+        // Imageview to show
+        ImageView image = (ImageView) findViewById(R.id.image);
+ 
+        // Image url
+        String image_url = "http://api.androidhive.info/images/sample.jpg";
+ 
+        // ImageLoader class instance
+        ImageLoader imgLoader = new ImageLoader(getApplicationContext());
+ 
+        // whenever you want to load an image from url
+        // call DisplayImage function
+        // url - image url to load
+        // loader - loader image, will be displayed before getting image
+        // image - ImageView
+        imgLoader.DisplayImage(image_url, loader, image);
+		
+//		WebView myWebView = (WebView) findViewById(R.id.webview);                   
+//		myWebView.loadUrl("http://img191.imageshack.us/img191/7379/tronlegacys7i7wsjf.jpg");
+        
+        
 	}
 
 	@Override
@@ -18,5 +45,25 @@ public class Profile extends Activity {
 		getMenuInflater().inflate(R.menu.activity_profile, menu);
 		return true;
 	}
+	
+    public void home(View view){
+    	Intent intent = new Intent(this, HomeScreen.class);
+    	startActivity(intent);    	
+    }
+	
+    public void explore(View view){
+    	Intent intent = new Intent(this, Explore.class);
+    	startActivity(intent);    	
+    }
+    
+    public void camera(View view){
+    	Intent intent = new Intent(this, Camera.class);
+    	startActivity(intent);    	
+    }
+    
+    public void settings(View view){
+    	Intent intent = new Intent(this, PrefsActivity.class);
+    	startActivity(intent);    	
+    }
 
 }
