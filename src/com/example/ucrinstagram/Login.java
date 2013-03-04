ec5397efe328dc69dbe187e7149c6bc3bcde2bc3
Oliver Chou <oliver@mgxcopy.com>
2013-03-03 20:27:21 -0800

    cleaned up login page, doesn't refresh, has a toast error message and uses the api

--- src/com/example/ucrinstagram/Login.java
+++ src/com/example/ucrinstagram/Login.java
@@ -1,40 +1,18 @@
 package com.example.ucrinstagram;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.widget.Toast;
+import com.example.ucrinstagram.Models.User;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 
 public class Login extends Activity {
-
 	public static String username="";
 	public static String password="";
-
-	InputStream is;
-	boolean Home = true;
-	boolean Finished = false;
-
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -50,94 +28,18 @@ public class Login extends Activity {
 	}
 
     public void HomeScreen(View view){
-    	Finished = false;
     	username = ((EditText)findViewById(R.id.member_username)).getText().toString();
     	password = ((EditText)findViewById(R.id.member_password)).getText().toString();
-		new checkLogin().execute();
-		while(!Finished);
 
-		if(Home){
-			Intent intent = new Intent(this, HomeScreen.class);
-			intent.putExtra("caption", "");
-			startActivity(intent);
-		}
-		else{
-			finish();
-			startActivity(getIntent());
-		}
+        User loginUser = new User(username);
+        if (loginUser == null){ // check username exists
+            Toast.makeText(this.getApplicationContext(), "Incorrect Username/Password", Toast.LENGTH_LONG).show();
+        } else if (loginUser.checkPassword(password)){ // check is password is correct
+            Intent intent = new Intent(this, HomeScreen.class);
+            intent.putExtra("caption", "");
+            startActivity(intent);
+        } else{
+            Toast.makeText(this.getApplicationContext(), "Incorrect Username/Password", Toast.LENGTH_LONG).show();
+        }
     }
-
-    private class checkLogin extends AsyncTask<Void,Void,Void>{
-		@Override
-		protected Void doInBackground(Void... arg0) {
-			String result = "";
-
-			ArrayList<NameValuePair> userinfo = new ArrayList<NameValuePair>();
-			userinfo.add(new BasicNameValuePair("user",username));
-			userinfo.add(new BasicNameValuePair("password",password));
-
-
-			//http post
-			try{
-			        HttpClient httpclient = new DefaultHttpClient();
-			        HttpPost httppost = new HttpPost("http://www.kevingouw.com/cs180/checkUser.php");
-			        httppost.setEntity(new UrlEncodedFormEntity(userinfo));
-			        HttpResponse response = httpclient.execute(httppost);
-			        HttpEntity entity = response.getEntity();
-			        is = entity.getContent();
-
-			}
-			catch(Exception e){
-			        Log.e("log_tag", "Error in http connection "+e.toString());
-			}
-			//convert response to string
-			try{
-
-			        BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
-			        StringBuilder sb = new StringBuilder();
-			        String line = null;
-			        while ((line = reader.readLine()) != null) {
-			                sb.append(line + "\n");
-			        }
-			        is.close();
-
-			        result=sb.toString();
-
-			}catch(Exception e){
-			        Log.e("log_tag", "Error converting result "+e.toString());
-			}
-			 System.out.println("======================");
-
-
-			//parse json data
-			try{
-			        JSONArray jArray = new JSONArray(result);
-			        for(int i=0;i<jArray.length();i++){
-			                JSONObject json_data = jArray.getJSONObject(i);
-			                //userinfo.add(json_data.getInt("id"));
-			                Home = true;
-			                System.out.println(json_data.getInt("id"));
-			                //System.out.println(json_data.getString("image_url"));
-			        }
-
-			}
-			catch(JSONException e){
-					Home = false;
-					System.out.println(Home);
-			        Log.e("log_tag", "Error parsing data "+e.toString());
-			}
-
-
-			 System.out.println("======================");
-			 //TextView tmp =(TextView)findViewById(R.id.errorlogin);
-			 //tmp.setText( "Incorrect username/password combination, please try again:");
-			 Finished = true;
-			return null;
-
-
-		}
-		protected void onPostExecute(Void Result){
-		}
-
-	}
 }
