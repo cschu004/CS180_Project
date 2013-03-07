731f79e39d9bd2697e24ef50319d73518389556d
Oliver Chou <oliver@mgxcopy.com>
2013-03-06 16:32:34 -0800

    2 new constructors for photo

--- src/com/example/ucrinstagram/Models/Photo.java
+++ src/com/example/ucrinstagram/Models/Photo.java
@@ -36,6 +36,28 @@ public class Photo {
     public Photo() {
     }
 
+    public Photo(String path, String filename) {
+        this.id = new Random().nextInt();
+        this.path = path;
+        this.filename = filename;
+        this.caption = "";
+        this.gps = "";
+        this.created_at = new Date();
+        this.updated_at = new Date();
+        this.deleted_at = new Date(0);
+    }
+
+    public Photo(String path, String filename, String caption) {
+        this.id = new Random().nextInt();
+        this.path = path;
+        this.filename = filename;
+        this.caption = caption;
+        this.gps = "";
+        this.created_at = new Date();
+        this.updated_at = new Date();
+        this.deleted_at = new Date(0);
+    }
+
     public Photo(String path, String filename, String caption, String gps) {
         this.id = new Random().nextInt();
         this.path = path;
