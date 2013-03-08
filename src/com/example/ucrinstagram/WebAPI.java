492522100702c63cec8e0399df040b1dd6b7fe16
Oliver Chou <oliver@mgxcopy.com>
2013-03-07 23:56:49 -0800

    getHomeScreenPhotos fix - missing url

--- src/com/example/ucrinstagram/WebAPI.java
+++ src/com/example/ucrinstagram/WebAPI.java
@@ -178,7 +178,7 @@ public class WebAPI {
     }
 
     public Photo[] getHomeScreenPhotos(int id){
-        String url = apiURL + User.urlSuffix + "/" + Integer.toString(id);
+        String url = apiURL + User.urlSuffix + "/get_home_screen_photos/" + Integer.toString(id);
         Log.i("OC", "Attempting to get home screen photos by user id: " + url);
         HTTPRequestMethod requestMethod = HTTPRequestMethod.GET;
         String json = getJSONFromServer(new HTTPParams(requestMethod, url));
@@ -216,7 +216,6 @@ public class WebAPI {
         return gson.fromJson(json, Comment[].class);
     }
     
-
     public void addCommentToPhoto(Photo photo, Comment comment){
         String url = apiURL + Photo.urlSuffix + "/add_comment/" + Integer.toString(photo.getId());
         Log.i("OC", "Attempting to add a Comment to Photo: " + url);
@@ -224,7 +223,6 @@ public class WebAPI {
         getJSONFromServer(new HTTPParams(requestMethod, url, comment.getNameValuePairs()));
     }
         
-    
     public void savePhoto(Photo photo) {
         String url = apiURL + Photo.urlSuffix + "/" + Integer.toString(photo.getId());
         Log.i("OC", "Attempting to edit Photo info: " + url);
